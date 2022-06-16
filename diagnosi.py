aac6e1ecd62c05eea65245c417ba45770a095f27
Luca Depalo <l.depalo8@studenti.uniba.it>
2022-06-16 18:13:15 +0200

    aggiunte per sistema esperto

--- diagnosi.py
+++ diagnosi.py
@@ -1,1 +1,10 @@
 from experta import *
+
+
+def valid_response(response: str):
+    valid = False
+    response = response.lower()
+    if response == "si" or response == "no":
+        valid = True
+    return valid
+
